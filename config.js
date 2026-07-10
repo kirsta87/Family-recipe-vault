4dd8941f2d2f846cb5e8528628815d4df682e3ea
kirsta87 <kirsta87@yahoo.com>
2026-07-10 14:32:25 -0600

    Load shared recipes on every device

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.RECIPE_VAULT_CONFIG = {
-  sheetCsvUrl: "",
-  appsScriptUrl: "",
+  familyName: "The Glaister Family",
+  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTuwOHFKZ5ZHyMlB4EuILRNLjQwAheIdDlU8ekIuo2BT-Nj1j04CyEoscZ4lCvpcxPpMu4UUeliD-Se/pub?gid=0&single=true&output=csv",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxeWg9Bdy8LgL7l1fRE0yd7i5s8IzXZQLEIshKzyqP0XASkVZkCqt3UM6Zhu4-NdpnD/exec",
   sharedKey: ""
 };
