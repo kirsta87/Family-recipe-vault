510ca3330fa939cc7cbe25f7fc2387f81d900ff9
kirsta87 <kirsta87@yahoo.com>
2026-07-27 09:09:23 -0600

    fix: sync build 201 versioning and cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const BUILD = 200;
+const BUILD = 201;
 const CACHE_NAME = `recipe-vault-v${BUILD}`;
 const APP_SHELL = [
   "./", "./index.html", `./styles.css?v=${BUILD}`, `./app.js?v=${BUILD}`,
