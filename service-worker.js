13dd91e14b6c93a1554ff1f6a9d1767aab45c632
kirsta87 <kirsta87@yahoo.com>
2026-07-28 18:54:36 -0600

    214

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const BUILD = 213;
+const BUILD = 214;
 const CACHE_NAME = `recipe-vault-v${BUILD}`;
 const APP_SHELL = [
   "./", "./index.html", `./styles.css?v=${BUILD}`, `./app.js?v=${BUILD}`,
